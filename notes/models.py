fe2b26423288d25ac1850ade439232111723a7c1
rezarinaldi <rez4rinaldi@gmail.com>
2025-04-15 07:38:27 +0700

    chore: day 4 done ✅

--- notes/models.py
+++ notes/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    create_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+class Task(models.Model):
+    task = models.TextField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    note = models.ForeignKey(Note,on_delete=models.CASCADE)
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Note
 
 # Create your views here.
 def index_view(request):
-    return render(request, "index.html")
+
+    notes = Note.objects.all()
+
+    return render(request, "index.html", {"notes": notes})
+
+def profile_view(request):
+
+    name = "Reza"
+
+    return render(request, "profile.html", {"name": name})
